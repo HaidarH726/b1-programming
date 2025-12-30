8dd6e176cced331ea035e1e0a2249ad916ced1a8
HaidarH726 <Haidar.almoudi96@gmail.com>
2025-12-30 12:20:51 +0100

    commit

--- week 5/exercise1.py
+++ week 5/exercise1.py
@@ -1,19 +1,52 @@
 # Password validation tool
-password = input("enter your password:")
-def check_password(password):
-    if password == "":
-        return True
-    if len(password) > 8:
-        return True
-    if password == "":
-        return True
-    if password.isdigit():
-        return True
-    if password.islower():
-        return True
-    if password.isupper():
-        return True
-    if password.isspecial():
-        return True
+
+import string
+import random
+# PART A
+def check_min_length(password, min_length=8):
+    return len(password) >= min_length
+def has_uppercase(password):
+    return any(char.isupper() for char in password)
+def has_lowercase(password):
+    return any(char.islower() for char in password)
+def has_digit(password):
+   return any(char.isdigit() for char in password)
+def has_special(password):
+    return any(char in string.punctuation for char in password)
+
+
+# PART B
+def validate_password(password):
+        results = {
+            "min_length": check_min_length(password),
+            "uppercase": has_uppercase(password),
+            "lowercase": has_lowercase(password),
+            "digits": has_digit(password),
+            "special": has_special(password)
+        }
+
+        results["is_valid"] = all(results.values())
+        return results
+#PART C
+def main():
+    password = input("Enter your password: ")
+    result = validate_password(password)
+
+    print("\nPassword check results:")
+    print("Minimum length:", "Met" if result["min_length"] else "Not met")
+    print("Has uppercase:", "Met" if result["uppercase"] else "Not met")
+    print("Has lowercase:", "Met" if result["lowercase"] else "Not met")
+    print("Has digit:", "Met" if result["digits"] else "Not met")
+    print("Has special character:", "Met" if result["special"] else "Not met")
+
+    if result["is_valid"]:
+        print("\nYour password is STRONG 💪")
     else:
-        return False
+        print("\nYour password is WEAK ❌")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
